69e6844cd23fd5c86189bd858544386a21ded962
Justice Rivera <142175716+justicetrace@users.noreply.github.com>
2024-06-19 23:57:36 +0000

    random card generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(randomSuit());
+  document.querySelector(".card").innerHTML = randomNumber();
+};
+let randomNumber = () => {
+  let num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * num.length);
+  return num[indexNumbers];
+};
+
+let randomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
